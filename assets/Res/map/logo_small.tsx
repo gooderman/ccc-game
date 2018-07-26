29e9e991dfde138dbbc5c6e81e25678507939938
jeep <jeep@users.noreply.github.com>
2018-07-26 20:04:18 +0800

    step

---
+++ assets/Res/map/logo_small.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="logo_small" tilewidth="32" tileheight="32" tilecount="21" columns="7">
+ <image source="logo_small.png" width="224" height="126"/>
+</tileset>
